cd648ca051427a93f031be5514832743d33c59a0
Alessandro5x <alessandro5@live.com>
2025-10-12 10:20:22 -0300

    feat: starting oo

---
+++ objects/oo.js
@@ -0,0 +1,22 @@
+// Non executable cod
+
+// Procedure
+process(value1, value2, value3)
+
+// OO
+object = {
+    value1,
+    value2,
+    value3,
+    process() {
+        // ...
+    }
+}
+
+object.process() // Focus now is the object
+
+// Important principles
+// 1. Abstraction
+// 2. Encapsulation
+// 3. Heritage (prototype)
+// 4. Polymorphism
