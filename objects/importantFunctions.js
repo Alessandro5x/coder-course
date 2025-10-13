4606d5e79478c362b300df4937c29229b2dac17c
Alessandro5x <alessandro5@live.com>
2025-10-13 09:45:48 -0300

    feat: important functions inside Obj

---
+++ objects/importantFunctions.js
@@ -0,0 +1,34 @@
+const person = {
+    name: 'Rebeca',
+    age: 2,
+    weight: 13
+}
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
+
+Object.entries(person).forEach(([key, value]) => {
+    console.log(`${key}: ${value}`)
+})
+
+Object.defineProperty(person, 'birthDate', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2019'
+})
+
+person.birthDate = '01/01/2017'
+console.log(person.birthDate)
+console.log(Object.keys(person))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1}
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4}
+const obj = Object.assign(dest, o1, o2)
+console.log(obj)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
